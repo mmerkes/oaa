a0a4e2250d6f31cabbd1290ab197b242c3991dcf
Ivan Storck <ivanoats@gmail.com>
2014-03-10 15:19:34 -0700

    clean up update and delete users routes because we don't need a responseUser, mongo does not provide one https://app.asana.com/0/10816721735449/10832665480121

--- routes/users.js
+++ routes/users.js
@@ -11,22 +11,22 @@ exports.collection = function(req, res) {
 exports.findById = function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   var id = req.params.id;
-  User.findOne({'_id': String(id)}, function(err, user) {
+  User.findOne({'_id': String(id)}, function(err, responseUser) {
     if(err) {
       res.send({'error': err});
     } else {
-      res.send(user);
+      res.send(responseUser);
     }
   });
 };
 
 exports.newUser = function(req, res) {
   var user = new User(req.body);
-  user.save(function(err, user) {
+  user.save(function(err, responseUser) {
     if(err){
       res.send({'error': err});
     } else {
-      res.send(user);
+      res.send(responseUser);
     }
   });
 };
@@ -34,7 +34,7 @@ exports.newUser = function(req, res) {
 exports.updateUser = function(req, res) {
   var id = req.params.id;
   var user = req.body;
-  User.update({'_id': String(id)}, user, function(err, resuser){
+  User.update({'_id': String(id)}, user, function(err){
     if(err) {
       res.send({'error': err});
     } else {
@@ -45,7 +45,7 @@ exports.updateUser = function(req, res) {
 
 exports.deleteUser = function(req, res) {
   var id = String(req.params.id);
-  User.remove({'_id': id}, function(err, userres){
+  User.remove({'_id': id}, function(err){
     if(err){
       res.send({'error': err});
     } else {
