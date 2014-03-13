30d89d65307751d586ecade7c00c41d44d1c7d41
Tyler Morgan <tylermorgan86@gmail.com>
2014-03-12 20:41:26 -0700

    basic model and view added

--- app/assets/js/browser.js
+++ app/assets/js/browser.js
@@ -7,11 +7,9 @@ var _          = require('underscore');
 var Backbone   = require('backbone');
 Backbone.$     = $;
 
-var User = Backbone.Model.extend({
-  idAttribute: "_id",
-  urlRoot: 'http://localhost:3000/api/v1/users'
-});
-
+var User = require('./models/User');
+var UserView = require('./views/UserView');
+$(function() {
 var user = new User({first_name: "Steve", last_name: "McQueen"});
 user.save({},{
   success: function(){
@@ -23,3 +21,8 @@ user.save({},{
   }
 });
 
+var userView = new UserView({model: user});
+console.log(userView.el);
+console.log('.container');
+$('.container').append(userView.el);
+});
