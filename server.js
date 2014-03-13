156b72e0d3b8ff09a1d4f15248a89cc2d2d3a256
Tyler Morgan <tylermorgan86@gmail.com>
2014-03-13 13:19:48 -0700

    redirecting users to use with backbone

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.put('/api/v1/users/:id', users.updateUser);
 
 app.delete('/api/v1/users/:id', users.deleteUser);
 
+app.get('/users*', function(req, res) {
+  res.redirect('/#users' + req.params)
+});
+
 var server = http.createServer(app);
 
 server.listen(3000);
